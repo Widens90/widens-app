17c01d70e8d1a0fcd7db3fb77d5e6266290c7c3a
Widens90 <alialitahsin90@gmail.com>
2026-08-15 16:58:38 +0200

    Fix typo in set_page_config function call

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.se st.set_page_config(
+st.set_page_config(
     page_title="Widens Åkeri AB", 
     page_icon="🚛", 
     layout="wide",
@@ -19,7 +19,6 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
 
 DATA_FILE = "drivers_data.csv"
 USERS_FILE = "users_data.csv"
